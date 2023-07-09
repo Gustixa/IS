086a5018ff8ded7bc39ef82ac8fa7625693bad71
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-07-08 22:16:44 -0600

    Actualizando el sideBar

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -7,13 +7,20 @@ import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
+import LogoutIcon from '@mui/icons-material/Logout'
 import IconButton from '@mui/material/IconButton'
-
+import Groups2Icon from '@mui/icons-material/Groups2'
 import AppBar  from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import MenuIcon from '@mui/icons-material/Menu'
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu'
+
+// Elementos del SideBar
+const buttonData = {
+  'Charlas Delva': Groups2Icon,
+  'Horas Beca': HistoryEduIcon
+}
+
 
 export default function SideBar(){
   const [state, setState] = useState({left: false})
@@ -31,12 +38,13 @@ export default function SideBar(){
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
+      {/* Se ajusta el tema de poder automatizar los elementos que aparecen en el sideBar */}
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {Object.keys(buttonData).map((text) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {React.createElement(buttonData[text])}
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
@@ -45,16 +53,15 @@ export default function SideBar(){
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        {/* En este caso, no es necesario automatizar, pues es solo 1 elemento */}
+        <ListItem key='Cerrar sesión' disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <LogoutIcon/>
+            </ListItemIcon>
+            <ListItemText primary='Cerrar sesión' />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   )
@@ -64,7 +71,7 @@ export default function SideBar(){
         <AppBar position="static">
           <Toolbar variant='dense'>
             <IconButton edge="start" color="inherit"
-              onClick={toggleDrawer('left', 'left')}
+              onClick={toggleDrawer('left', true)}
             >
               <MenuIcon/>
             </IconButton>
