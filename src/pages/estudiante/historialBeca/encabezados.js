bec2fca0cc0248572695f125bb635515238a27f1
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2023-07-27 13:09:58 -0600

    Update encabezados.js

--- src/pages/estudiante/historialBeca/encabezados.js
+++ src/pages/estudiante/historialBeca/encabezados.js
@@ -2,7 +2,7 @@ const encabezados = [
     "Actividad realizada",
     "Fecha",
     "horas acreditadas",
-    "horas faltates","acumulado años anteriores", "Horas realizadas", "Horas por realizar"
+    "horas pendientes","acumulado años anteriores", "Horas realizadas", "Horas por realizar"
 ]
 
 export default encabezados
