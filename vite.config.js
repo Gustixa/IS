7f713e2c3a29d3b7293b89ae20b7bb5dcf7bdb33
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2023-08-10 23:26:46 -0600

    Incluyendo Vitest

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+  },
   plugins: [react()],
   resolve: {
     alias: [
