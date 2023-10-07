9dd16d071445e9477a34310767f1d949a8797d64
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-10-06 18:53:46 -0600

    Modificacion para probar ci-cd

--- src/pages/estudiante/historialBeca/encabezados.js
+++ src/pages/estudiante/historialBeca/encabezados.js
@@ -6,6 +6,6 @@ const encabezados = [
 ]
 
 const encabezado_seccion_principal = [
-    "Registro de actividades de horas realizadas"
+    "Registro de actividades de horas realizadas en el año"
 ]
 export  {encabezados, encabezado_seccion_principal}
